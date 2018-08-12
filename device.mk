029a61d22a01ae27f479e4e86011803d71d5c595
LuK1337 <priv.luk@gmail.com>
2018-08-12 11:49:14 +0200

    s2: Disable telephony-ext for now
    Change-Id: Ia0ae303a786640f2535cae0ab2dfcd041569bca4

--- device.mk
+++ device.mk
@@ -338,11 +338,11 @@ PRODUCT_PACKAGES += \
     libstlport
 
 # Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
+# PRODUCT_PACKAGES += \
+#    telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+# PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \
